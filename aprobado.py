f36bb024f3ed169708e7998228841d61ee8fa4ee
PilarMil <2015200473@untels.edu.pe>
2020-07-09 14:58:33 -0500

    Metodo para determinar aprobacion

---
+++ aprobado.py
@@ -0,0 +1,9 @@
+#ejecicio que determinar si el estudiante esta aprobado o no..
+#autor Pilar Navarro
+
+def determinaraprobador(promedio):
+    if promedio>=11:
+        resultado = "Aprobado"
+    else:
+        resultado = "Desaprobado"
+    return resultado
